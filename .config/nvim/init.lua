57fa543c11b9b1e9accfa4eaba8f1b0472701c6c
Mulling <lmulling@proton.me>
2023-10-26 17:18:00 -0300

    Update telescope version

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -30,7 +30,7 @@ packer.startup(function()
 
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
-    use { 'nvim-telescope/telescope.nvim', tag = '0.1.0', requires = { { 'nvim-lua/plenary.nvim' } } }
+    use { 'nvim-telescope/telescope.nvim', tag = '0.1.4', requires = { { 'nvim-lua/plenary.nvim' } } }
 
     use { 'hrsh7th/nvim-cmp', requires = { { 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip' } } }
 
